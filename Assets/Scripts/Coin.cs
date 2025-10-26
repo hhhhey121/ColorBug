b57a6ddf04e49eb0db10b4dcf4c91f7430572470
hhhhey121 <15367107726@163.com>
2025-10-26 20:09:54 +0800

    增加通关要求，修改第三关地刺bug

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -8,14 +8,27 @@ public class Coin : MonoBehaviour
     private int coins = 0;
 
     [SerializeField] private Text coinText;//实例化
-                                           
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))//碰到coin标签时
         {
             Destroy(collision.gameObject);//销毁coin
             coins++;
-            coinText.text = "Coins:" + coins;
+
+            // 确保 coinText 已经被拖拽赋值
+            if (coinText != null)
+            {
+                coinText.text = "Coins:" + coins;
+            }
         }
+    }
+
+    // 【新增】
+    // 添加一个公共方法 (public method)
+    // 这样其他脚本 (比如 Finish.cs) 就可以调用它来获取金币数量
+    public int GetCoinCount()
+    {
+        return coins;
     }
 }
--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;//跟场景有关的要写
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    private AudioSource finishSound;//结束音频 （后期添加
+    private AudioSource finishSound;
+
+    // 【新增】在Unity的Inspector中设置本关需要多少金币
+    // 比如：第一关设为 1，之后的关卡设为 3
+    public int requiredCoins = 1;
+
+    // 【新增】(可选) 金币不够时播放的音效
+    public AudioClip lockedSound;
+
     void Start()
     {
-        finishSound=GetComponent<AudioSource>();
+        finishSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name=="Player")
+        // 【建议】最好使用标签(Tag)来检查玩家
+        if (collision.gameObject.CompareTag("Player"))
         {
-            finishSound.Play();
-            finishLevel();
+            // 1. 尝试从碰撞到的玩家(collision.gameObject)身上获取 Coin 脚本
+            Coin playerCoinCollector = collision.gameObject.GetComponent<Coin>();
+
+            if (playerCoinCollector != null)
+            {
+                // 2. 检查金币数量是否达标
+                if (playerCoinCollector.GetCoinCount() >= requiredCoins)
+                {
+                    // 金币足够，通关
+                    finishSound.Play();
+                    finishLevel();
+                }
+                else
+                {
+                    // 金币不够
+                    Debug.Log("金币不足! 需要: " + requiredCoins + ", 当前: " + playerCoinCollector.GetCoinCount());
+                    if (lockedSound != null && !finishSound.isPlaying)
+                    {
+                        finishSound.PlayOneShot(lockedSound);
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("在玩家身上没有找到 Coin.cs 脚本!");
+            }
         }
     }
+
     private void finishLevel()//关卡结束
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
